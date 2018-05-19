fcdd55eeac193cafee0545f07ae77af849552629
Alex Thomson <LXGaming@users.noreply.github.com>
2018-05-19 14:17:19 +1200

    Alternative AdvancementProgress fix
    Setting the progressMap to null may have been causing advancement messages to get spammed in chat, an alternative fix has been developed which should hopefully avoid this issue.

--- src/main/java/io/github/lxgaming/sledgehammer/mixin/core/advancements/MixinAdvancementProgress.java
+++ src/main/java/io/github/lxgaming/sledgehammer/mixin/core/advancements/MixinAdvancementProgress.java
@@ -18,7 +18,9 @@ package io.github.lxgaming.sledgehammer.mixin.core.advancements;
 
 import io.github.lxgaming.sledgehammer.Sledgehammer;
 import net.minecraft.advancements.AdvancementProgress;
+import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.api.advancement.criteria.AdvancementCriterion;
+import org.spongepowered.api.advancement.criteria.OperatorCriterion;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,6 +32,7 @@ import org.spongepowered.common.interfaces.advancement.IMixinAdvancementProgress
 import org.spongepowered.common.interfaces.advancement.IMixinPlayerAdvancements;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Mixin(value = AdvancementProgress.class, priority = 1337)
 public abstract class MixinAdvancementProgress implements org.spongepowered.api.advancement.AdvancementProgress, IMixinAdvancementProgress {
@@ -37,12 +40,62 @@ public abstract class MixinAdvancementProgress implements org.spongepowered.api.
     @Dynamic(mixin = org.spongepowered.common.mixin.core.advancement.MixinAdvancementProgress.class)
     private Map<AdvancementCriterion, ICriterionProgress> progressMap;
     
-    @Inject(method = "isDone", at = @At(value = "HEAD"))
+    @Inject(method = "isDone", at = @At(value = "HEAD"), cancellable = true)
     private void onIsDone(CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        /*
+        // Setting the progressMap to null might be the cause of the advancement message spam
         if (!get(getAdvancement().getCriterion()).isPresent()) {
             this.progressMap = null;
             Player player = ((IMixinPlayerAdvancements) getPlayerAdvancements()).getPlayer();
             Sledgehammer.getInstance().debugMessage("Reset {} for {} ({})", getAdvancement().getCriterion().getName(), player.getName(), player.getUniqueId());
         }
+        */
+        
+        if (progressMap == null || progressMap.isEmpty()) {
+            callbackInfoReturnable.setReturnValue(false);
+            return;
+        }
+        
+        AdvancementCriterion advancementCriterion = getAdvancement().getCriterion();
+        Player player = ((IMixinPlayerAdvancements) getPlayerAdvancements()).getPlayer();
+        
+        Optional<ICriterionProgress> criterionProgress;
+        if (advancementCriterion instanceof OperatorCriterion) {
+            criterionProgress = getCriterionProgress((OperatorCriterion) advancementCriterion);
+        } else {
+            criterionProgress = getCriterionProgress(advancementCriterion);
+        }
+        
+        callbackInfoReturnable.setReturnValue(criterionProgress.map(ICriterionProgress::achieved).orElse(false));
+        
+        if (!criterionProgress.isPresent()) {
+            Sledgehammer.getInstance().debugMessage("Failed to find {} for {} ({})", advancementCriterion.getName(), player.getName(), player.getUniqueId());
+        }
+    }
+    
+    private Optional<ICriterionProgress> getCriterionProgress(AdvancementCriterion advancementCriterion) {
+        for (Map.Entry<AdvancementCriterion, ICriterionProgress> entry : progressMap.entrySet()) {
+            if (entry.getKey() instanceof OperatorCriterion) {
+                continue;
+            }
+            
+            if (StringUtils.equals(advancementCriterion.getName(), entry.getKey().getName())) {
+                return Optional.of(entry.getValue());
+            }
+        }
+        
+        return Optional.empty();
+    }
+    
+    private Optional<ICriterionProgress> getCriterionProgress(OperatorCriterion operatorCriterion) {
+        for (Map.Entry<AdvancementCriterion, ICriterionProgress> entry : progressMap.entrySet()) {
+            if (!(entry.getKey() instanceof OperatorCriterion)) {
+                continue;
+            }
+            
+            return Optional.of(entry.getValue());
+        }
+        
+        return Optional.empty();
     }
 }
