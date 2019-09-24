6af9f5c8894209adfefe63bfd3657f80f4ea0a1d
Alex Thomson <LXGaming@users.noreply.github.com>
2019-09-24 18:14:46 +1200

    Default to Item in players hand

--- src/main/java/io/github/lxgaming/sledgehammer/mixin/notreepunching/ModEventHandlerMixin.java
+++ src/main/java/io/github/lxgaming/sledgehammer/mixin/notreepunching/ModEventHandlerMixin.java
@@ -17,29 +17,42 @@
 package io.github.lxgaming.sledgehammer.mixin.notreepunching;
 
 import com.alcatrazescapee.notreepunching.ModEventHandler;
-import com.alcatrazescapee.notreepunching.common.capability.IPlayerItem;
+import com.alcatrazescapee.notreepunching.util.HarvestBlockHandler;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.List;
+
 @Mixin(value = ModEventHandler.class, priority = 1337, remap = false)
 public abstract class ModEventHandlerMixin {
     
-    /**
-     * no-tree-punching doesn't check if the ItemStack contained in the Capability is null.
-     */
     @Redirect(method = "harvestBlock",
             at = @At(value = "INVOKE",
-                    target = "Lcom/alcatrazescapee/notreepunching/common/capability/IPlayerItem;getStack()Lnet/minecraft/item/ItemStack;"
+                    target = "Lcom/alcatrazescapee/notreepunching/util/HarvestBlockHandler;addExtraDrops(Ljava/util/List;Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/item/ItemStack;)V"
             )
     )
-    private static ItemStack onGetStack(IPlayerItem playerItem) {
-        ItemStack itemStack = playerItem.getStack();
-        if (itemStack != null) {
-            return itemStack;
+    private static void onAddExtraDrops(List<ItemStack> drops, IBlockState state, EntityPlayer player, ItemStack stack) {
+        if (stack != null) {
+            HarvestBlockHandler.addExtraDrops(drops, state, player, stack);
+        } else {
+            HarvestBlockHandler.addExtraDrops(drops, state, player, player.getHeldItemMainhand());
         }
-        
-        return ItemStack.EMPTY;
+    }
+    
+    @Redirect(method = "harvestBlock",
+            at = @At(value = "INVOKE",
+                    target = "Lcom/alcatrazescapee/notreepunching/util/HarvestBlockHandler;isInvalidTool(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/block/state/IBlockState;)Z"
+            )
+    )
+    private static boolean onIsInvalidTool(ItemStack stack, EntityPlayer player, IBlockState state) {
+        if (stack != null) {
+            return HarvestBlockHandler.isInvalidTool(stack, player, state);
+        } else {
+            return HarvestBlockHandler.isInvalidTool(player.getHeldItemMainhand(), player, state);
+        }
     }
 }
