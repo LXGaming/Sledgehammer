0902dd290518261eaeda63136ce5b11dcb2c044a
Alex Thomson <LXGaming@users.noreply.github.com>
2021-08-05 19:22:21 +1200

    Remove unused class

--- src/main/java/io/github/lxgaming/sledgehammer/mixin/core/world/WorldEntitySpawnerMixin_DespawnRanges.java
+++
@@ -1,8 +0,0 @@
-package io.github.lxgaming.sledgehammer.mixin.core.world;
-
-import net.minecraft.world.WorldEntitySpawner;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin( WorldEntitySpawner.class )
-public class WorldEntitySpawnerMixin_DespawnRanges {
-}
